bf5969fe82183494a08a8c062a6628db7a0f1c06
kyano7 <kyanon7@gmail.com>
2020-12-16 00:21:22 +0900

    first commit

---
+++ chapter01/src/main/java/org/eternity/theater/step02/Audience.java
@@ -0,0 +1,13 @@
+package org.eternity.theater.step02;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag){
+        this.bag = bag;
+    }
+
+    public Bag getBag(){
+        return bag;
+    }
+}
