c1c3a5f900e504dd2a9f098148d5b348f0b1070c
kyanon7 <kyanon7@gmail.com>
2020-12-17 20:28:52 +0900

    a

--- chapter02/src/main/java/org/eternity/movie/Screening.java
+++ chapter02/src/main/java/org/eternity/movie/Screening.java
@@ -7,10 +7,18 @@ public class Screening {
     private int sequence;
     private LocalDateTime whenScreened;
 
+    private Money calculateFee(int audienceCount){
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+
     public Screening(Movie movie, int sequence, LocalDateTime whenScreened){
         this.movie = movie;
         this.sequence = sequence;
         this.whenScreened = whenScreened;
+    }
+
+    public Reservation reserve(Customer customer, int audienceCount){
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
     }
 
     public LocalDateTime getSrartTime(){
