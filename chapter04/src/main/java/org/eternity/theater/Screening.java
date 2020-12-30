bbfe3631ac7329c11b3c21f2db7a1b7a81b5017a
kyanon7 <kyanon7@gmail.com>
2020-12-30 19:56:14 +0900

    챕터4 끝

--- chapter04/src/main/java/org/eternity/theater/Screening.java
+++ chapter04/src/main/java/org/eternity/theater/Screening.java
@@ -7,6 +7,12 @@ public class Screening {
     private int sequence;
     private LocalDateTime whenScreened;
 
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
     public Movie getMovie() {
         return movie;
     }
@@ -29,5 +35,23 @@ public class Screening {
 
     public void setWhenScreened(LocalDateTime whenScreened) {
         this.whenScreened = whenScreened;
+    }
+
+    public Money calculateFee(int audienceCount){
+        switch(movie.getMovieType()){
+            case AMOUNT_DISCOUNT:
+            if(movie.isDiscountable(whenScreened, sequence)){
+                return movie.calculateAmountDiscountedFee().times(audienceCount);
+            }
+            break;
+            case PERCENT_DISCOUNT:
+            if(movie.isDiscountable(whenScreened, sequence)){
+                return movie.calculateNoneDiscountedFee().times(audienceCount);
+            }
+            case NONE_DISCOUNT:
+            return movie.calculateNoneDiscountedFee().times(audienceCount);
+        }
+
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
     }
 }
