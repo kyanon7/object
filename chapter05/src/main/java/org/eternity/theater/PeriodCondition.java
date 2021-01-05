d494d945015280162891033c522ec5f4658bcb0b
kyanon7 <kyanon7@gmail.com>
2021-01-05 19:04:30 +0900

    챕터5 중간

---
+++ chapter05/src/main/java/org/eternity/theater/PeriodCondition.java
@@ -0,0 +1,22 @@
+package org.eternity.theater;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class PeriodCondition implements DiscountCondition{
+    private DayOfWeek dayOfWeek;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public PeriodCondition(DayOfWeek dayOfWeek, LocalDateTime startTime, LocalDateTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public boolean isSatisfiedBy(Screening screening){
+        return dayOfWeek.equals(screening.getWhenScreened().getDayOfWeek()) &&
+                startTime.compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
+                endTime.compareTo(screening.getWhenScreened().toLocalTime()) >= 0;
+    }
+}
---
+++ chapter05/src/main/java/org/eternity/theater/Screening.java
@@ -0,0 +1,26 @@
+package org.eternity.theater;
+
+import java.time.LocalDateTime;
+
+public class Screening{
+
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public LocalDateTime getWhenScreened() {
+        return whenScreened;
+    }
+
+    public Reservation reserve(Customer customer, int audience){
+    }
+
+    public Money calculateFee(int audience){
+        return movie.calculateMovieFee(this).times(audience);
+    }
+
+}
